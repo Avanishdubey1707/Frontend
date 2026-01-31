310d00642c8634a2c9303819eb9a48a8ab5845f6
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-01-31 18:50:03 +0530

    Intorduction to JavaScript

---
+++ Javascript/script.js
@@ -0,0 +1,17 @@
+alert("hello world");
+ 
+
+console.log("code is running")
+console.log("code is  also running")
+console.log("code is running good")
+
+var a = prompt("Enter your number")
+var isTrue=confirm("are you sure want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+
+}
+else{
+    console.log("computer is not blasting ")
+}
