b897121e018f872f21f1f1d5aa753c3ba5cf7307
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-01 13:07:30 +0530

    Document object model

---
+++ Javascript/new.js
@@ -0,0 +1,9 @@
+ console.log("DOM")
+
+ let dom=document.getElementsByClassName("box")
+ console.log(dom)
+
+ dom[2].style.backgroundColor="red"
+ document.getElementById("pu").style.backgroundColor="green"
+
+ 
