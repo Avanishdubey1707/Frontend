c793dd1604c6a0d1e6efb2ae50965c8c5240bae4
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-02 23:04:09 +0530

    function

--- Javascript/new.js
+++ Javascript/new.js
@@ -1,9 +1,13 @@
- console.log("DOM")
+//  console.log("DOM")
 
- let dom=document.getElementsByClassName("box")
- console.log(dom)
+//  let dom=document.getElementsByClassName("box")
+//  console.log(dom)
 
- dom[2].style.backgroundColor="red"
- document.getElementById("pu").style.backgroundColor="green"
+//  dom[2].style.backgroundColor="red"
+//  document.getElementById("pu").style.backgroundColor="green"
 
- 
+ function sum(a,b){
+    return a+b;
+
+ }
+ console.log(sum(3,4))
