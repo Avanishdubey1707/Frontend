2283f512d6081ef3b3ad18c810a0df3ade81a711
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-08 17:02:39 +0530

    Started to learn ReactJs

---
+++ ReactJs/textutils/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "Putin"
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Contact</li>
+      <li>About</li>
+    </nav>
+    <h1>{name}</h1>
+    <div className="blank">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, debitis enim at minus obcaecati corporis porro quaerat placeat natus earum iste maxime cumque totam alias maiores molestiae sit explicabo, eveniet eius a ipsa. Sed, error sequi assumenda vero modi eligendi mollitia in eveniet adipisci enim, vel ab similique accusamus, temporibus debitis labore maiores distinctio reprehenderit voluptate a rem molestias minus? Soluta repudiandae facilis aut beatae?</div>
+
+    </>
+  );
+}
+
+export default App;
