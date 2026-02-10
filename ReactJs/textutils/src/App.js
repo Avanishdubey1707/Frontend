0a335e366dd5e9bbf32e8ee85bba1b7d81a4dc53
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-10 16:39:16 +0530

    improving the dark mode function

--- ReactJs/textutils/src/App.js
+++ ReactJs/textutils/src/App.js
@@ -1,14 +1,25 @@
 
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 
+
 function App() {
+  const [mode,setMode] = useState('dark');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
    <>
-   <Navbar  title="TextUtils" aboutText="About"/>
+   <Navbar  title="TextUtils"  aboutText="About" mode={mode} toggleMode={toggleMode}/>
    <div className="container my-3">
    <TextForm heading="Enter The text to analyze"/>
    {/* <About/> */}
