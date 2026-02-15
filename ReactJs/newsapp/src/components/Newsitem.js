13276e662b6cbad45ae41b309b43fc59406f754c
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-16 02:58:43 +0530

    used fetch API to populate newsItems

--- ReactJs/newsapp/src/components/Newsitem.js
+++ ReactJs/newsapp/src/components/Newsitem.js
@@ -7,7 +7,7 @@ export class Newsitem extends Component {
             <div className="my-3">
                 
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200,q_50/lsci/db/PICTURES/CMS/400200/400228.jpg":imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
