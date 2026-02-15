54f290a8095b681183c598f8bf7f8a59dc00b944
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-02-16 00:50:32 +0530

    Added newsitemcard

--- ReactJs/newsapp/src/components/News.js
+++ ReactJs/newsapp/src/components/News.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-
+import Newsitem from './Newsitem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is the news component 
+      <div className="container my-3">
+           <h2>NewsMonkey - Top Headlines</h2>
+           <div className="row">
+               <div className="col-md-4">
+                    <Newsitem title="mytitle" description="mydesc"/>
+               </div>
+                <div className="col-md-4">
+                    <Newsitem title="mytitle" description="mydesc"/>
+               </div>
+                <div className="col-md-4">
+                    <Newsitem title="mytitle" description="mydesc"/>
+               </div>
+
+           </div>
+         
+        
       </div>
     )
   }
